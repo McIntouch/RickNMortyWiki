939015a4503b959ec86b690a515ead1fb43a1a59
Pongthorn Chumpoo <chumpoo.dev@gmail.com>
2023-12-03 23:33:48 +0700

    first commit

---
+++ RickNMortyWiki/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickNMortyWiki
+//
+//  Created by Pongthorn Chumpoo on 3/12/2566 BE.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
