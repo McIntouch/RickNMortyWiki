ce1666aa1dec07a73212ad251b52bcb8f325d1e8
Pongthorn Chumpoo <chumpoo.dev@gmail.com>
2023-12-11 21:06:46 +0700

    load more characters fron next pages into the collection view

--- RickNMortyWiki/APIClients/RMRequest.swift
+++ RickNMortyWiki/APIClients/RMRequest.swift
@@ -9,19 +9,19 @@ import Foundation
 
 /// a class to form an API path
 final class RMRequest {
-    struct Constant {
+    struct Constants {
         static let baseURL = "https://rickandmortyapi.com/api/"
     }
     let endpoint:RMEndpoint
-    var pathComponent:[String]
+    var pathComponents:[String]
     var queryParameters: [URLQueryItem]
     
     var urlString: String {
-        var string = Constant.baseURL
+        var string = Constants.baseURL
         string += endpoint.rawValue
         
-        if !pathComponent.isEmpty {
-            pathComponent.forEach({
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
                 string += "/\($0)"
             })
         }
@@ -44,10 +44,57 @@ final class RMRequest {
     /// http method
     public let httpMethod = "GET"
     
-    init(endpoint: RMEndpoint, pathComponent: [String] = [] , queryParameters: [URLQueryItem] = []) {
+    init(endpoint: RMEndpoint, pathComponents: [String] = [] , queryParameters: [URLQueryItem] = []) {
         self.endpoint = endpoint
-        self.pathComponent = pathComponent
+        self.pathComponents = pathComponents
         self.queryParameters = queryParameters
+    }
+    
+    convenience init?(url:URL){
+        let string = url.absoluteString
+                if !string.contains(Constants.baseURL) {
+                    return nil
+                }
+                let trimmed = string.replacingOccurrences(of: Constants.baseURL, with: "")
+                if trimmed.contains("/") {
+                    let components = trimmed.components(separatedBy: "/")
+                    if !components.isEmpty {
+                        let endpointString = components[0] // Endpoint
+                        var pathComponents: [String] = []
+                        if components.count > 1 {
+                            pathComponents = components
+                            pathComponents.removeFirst()
+                        }
+                        if let rmEndpoint = RMEndpoint(rawValue: endpointString) {
+                            self.init(endpoint: rmEndpoint, pathComponents: pathComponents)
+                            return
+                        }
+                    }
+                } else if trimmed.contains("?") {
+                    let components = trimmed.components(separatedBy: "?")
+                    if !components.isEmpty, components.count >= 2 {
+                        let endpointString = components[0]
+                        let queryItemsString = components[1]
+                        // value=name&value=name
+                        let queryItems: [URLQueryItem] = queryItemsString.components(separatedBy: "&").compactMap({
+                            guard $0.contains("=") else {
+                                return nil
+                            }
+                            let parts = $0.components(separatedBy: "=")
+
+                            return URLQueryItem(
+                                name: parts[0],
+                                value: parts[1]
+                            )
+                        })
+
+                        if let rmEndpoint = RMEndpoint(rawValue: endpointString) {
+                            self.init(endpoint: rmEndpoint, queryParameters: queryItems)
+                            return
+                        }
+                    }
+                }
+                return nil
     }
 }
 
--- RickNMortyWiki/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickNMortyWiki/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-final class RMCharacterCollectionViewCellViewModel {
+final class RMCharacterCollectionViewCellViewModel: Hashable,Equatable {
     public let characterName: String
     private let characterStatus: RMCharacterStatus
     private let characterImageURL: URL?
@@ -37,5 +37,17 @@ final class RMCharacterCollectionViewCellViewModel {
         }
         task.resume()
     }
+    
+    // MARK: - Hashable
+
+        static func == (lhs: RMCharacterCollectionViewCellViewModel, rhs: RMCharacterCollectionViewCellViewModel) -> Bool {
+            return lhs.hashValue == rhs.hashValue
+        }
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(characterName)
+            hasher.combine(characterStatus)
+            hasher.combine(characterImageURL)
+        }
     
 }
